e39d4694105ad7f31ab8b6b75a52b25fa3796d41
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-02-15 04:37:24 +0900

    feat: 공통 fetch 함수 추가

---
+++ src/utils/fetchDatas.ts
@@ -0,0 +1,11 @@
+export async function fetchData(url: string) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+  return null;
+}
