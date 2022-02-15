368972f2f49aaa769377e349b0091ac3067de44e
Seoyoung Jeong <issyo1022@gmail.com>
2022-02-16 00:03:21 +0900

    feat: ItemWarning 컴포넌트 추가

---
+++ src/components/organisms/ItemWarning/style.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  padding: 10px;
+`;
+
+export const Title = styled.span`
+  font-size: 14px;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-size: 12px;
+  color: #808080;
+  line-height: 1.4;
+`;
