f7ab874874a5a46a1f99fd6f07592d14f6db71ee
jambottle <jaewon.walks@gmail.com>
2022-02-16 05:10:16 +0900

    feat: Main 페이지에 삽입될 MainDiscount 컴포넌트 레이아웃 및 기능 구현 - Main 페이지의 중단에 위치시켜 땡처리 상품 목록을 Card에 바인딩 후, 리스트 렌더링으로 반복 - props로 받아온 itemList의 정적 타이핑을 위해 IMenuProps 타입에 필요한 항목 추가

---
+++ src/components/organisms/MainDiscount/index.tsx
@@ -0,0 +1,40 @@
+import { getItemList } from 'utils';
+import { IMenuProps } from 'types';
+import * as S from './style';
+
+export const MainDiscount = ({ itemList }: { itemList: Array<IMenuProps> }) => {
+  return (
+    <S.Wrapper data-test="main-discount">
+      <span>놓치지 마세요</span>
+      <h4>오늘의 땡처리콘!</h4>
+
+      {itemList.map((item) => (
+        <S.Card key={item.id}>
+          <div
+            className="card-image"
+            data-test="discount-image"
+            style={{ backgroundImage: `url(${item.imageUrl})` }}
+          />
+          <div className="card-info">
+            <span data-test="discount-brand">{item.conCategory2.name}</span>
+            <h6 data-test="discount-item">{item.name}</h6>
+            <span className="discount-rate" data-test="discount-rate">
+              {item.discountRate}%
+            </span>
+            <strong data-test="discount-price">{item.ncSellingPrice.toLocaleString()}원</strong>
+            <del data-test="original-pricd">{item.originalPrice.toLocaleString()}원</del>
+          </div>
+        </S.Card>
+      ))}
+    </S.Wrapper>
+  );
+};
+
+export async function getStaticProps() {
+  const [itemList] = await Promise.all([getItemList()]);
+  return {
+    props: {
+      itemList,
+    },
+  };
+}
