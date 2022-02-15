0c58821a76230674f693f214ca970b3d45b571ee
gml9812 <hee.jun.yun@gmail.com>
2022-02-15 14:15:18 +0900

    fix: min-height: 100vh 부여해 페이지 길이가 브라우저 높이보다 짧을 경우 회색 background가 잘리는 현상 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ const theme = {
 
 const LayoutWrapper = styled.div`
   width: 100%;
+  min-height: 100vh;
   height: 100%;
 `;
 
@@ -29,28 +30,6 @@ const Container = styled.div`
   }
 `;
 
-// 임시
-const Navbar = styled.div`
-  line-height: 1.15;
-  -webkit-text-size-adjust: 100%;
-  font-size: 15px;
-  font-family: Apple SD Gothic Neo, sans-serif;
-  font-weight: 600;
-  color: #000000;
-  -webkit-box-direction: normal;
-  box-sizing: border-box;
-  position: fixed;
-  display: flex;
-  background-color: #fff;
-  height: 59px;
-  z-index: 50;
-  width: 373px;
-  max-width: 48rem;
-  border-bottom: 1px solid transparent;
-  padding: 0px;
-  margin: 0px;
-`;
-
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
   return <Container>{children}</Container>;
 };
@@ -62,7 +41,6 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       <ThemeProvider theme={theme}>
         <LayoutWrapper>
           <AppLayout>
-            <Navbar />
             <Component {...pageProps} />
           </AppLayout>
         </LayoutWrapper>
