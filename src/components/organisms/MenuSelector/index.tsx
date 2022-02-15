88a1cdd1c38b0ea2859c3e9d90aa9740ea240ca2
gml9812 <hee.jun.yun@gmail.com>
2022-02-15 21:23:16 +0900

    feat: MenuSelector에 Drag 기능 추가

--- src/components/organisms/MenuSelector/index.tsx
+++ src/components/organisms/MenuSelector/index.tsx
@@ -1,3 +1,5 @@
+import { useState, useRef } from 'react';
+
 import * as S from './style';
 
 interface IMenuSelectorProps {
@@ -11,10 +13,38 @@ interface IMenuSelectorProps {
 }
 
 export const MenuSelector = ({ title = '', menuList, selected, onClick }: IMenuSelectorProps) => {
+  let isDragging = false;
+  let swipeStartPos = 0;
+  const slideRef = useRef<HTMLDivElement>(null);
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLDivElement>) => {
+    isDragging = true;
+    swipeStartPos = e.pageX;
+  };
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!isDragging) return;
+    const swipeLength = e.pageX - swipeStartPos;
+    if (slideRef.current) {
+      slideRef.current.style.transform = `translateX(${Math.min(swipeLength, 0)}px)`;
+    }
+  };
+
+  const handleMouseUp = () => {
+    isDragging = false;
+    swipeStartPos = 0;
+  };
+
   return (
     <S.Container>
       {title !== '' && <S.Title>{title}</S.Title>}
-      <S.MenuButtons>
+      <S.MenuButtons
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+        onMouseOut={handleMouseUp}
+        ref={slideRef}
+      >
         {menuList.map((menu) => (
           <S.Button key={menu.id} underLine={menu.id === selected} onClick={() => onClick(menu.id)}>
             {menu.name}
