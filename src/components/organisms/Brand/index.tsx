2722ed1f7617f9481bc565e6ec18ef67e9ba407b
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-02-15 17:08:36 +0900

    feat: setting brand components

---
+++ src/components/organisms/Brand/index.tsx
@@ -0,0 +1,5 @@
+import * as S from './style';
+
+export const Brand = () => {
+  return <S.Container />;
+};
---
+++ src/components/organisms/Brand/style.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`;
