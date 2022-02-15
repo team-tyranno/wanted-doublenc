8fd1a19ea2e7d1b08fa0ee751532df12d17f82e6
jambottle <jaewon.walks@gmail.com>
2022-02-16 02:20:43 +0900

    feat: Main 페이지에 NavBar 컴포넌트를 포함한 문서 구조 구성 - Main 페이지를 크게 다섯 개의 영역으로 나누고, 각각의 영역마다 data-test 속성으로 구분 - useEffect를 활용하여 Main 페이지에서 필요한 API Call 메서드 구현 및 콘솔 테스트 시도 - Carousel 컴포넌트에 쓰일 임시 이미지 파일을 로컬에서 불러와 해당 영역에 삽입 등

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,54 @@
+import { useEffect } from 'react';
 import type { NextPage } from 'next';
+import styled from 'styled-components';
+
+import { getCategoryList, getItemList } from 'utils';
+import { NavBar, Caret } from 'components';
+
+export const Button = styled.button`
+  display: flex;
+  overflow: visible;
+
+  margin: 0;
+  border: none;
+  box-shadow: none !important;
+  padding: 19px;
+  background-color: #fff;
+  text-transform: none;
+  cursor: pointer;
+
+  -webkit-appearance: button;
+  -webkit-box-direction: normal;
+`;
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    getCategoryList();
+    getItemList();
+  }, []);
+
   return (
-    <div>
-      <div>ds</div>
-    </div>
+    <>
+      <NavBar
+        data-test="main-header"
+        leftButton={
+          <Button type="button">
+            <Caret direction="left" />
+          </Button>
+        }
+        title="니콘내콘"
+      />
+
+      <section data-test="main-carousel">
+        <img alt="" src="/images/main-carousel-1.png" style={{ height: '141px' }} />
+      </section>
+
+      <section data-test="main-category" />
+
+      <section data-test="main-discount" />
+
+      <footer data-test="main-footer" />
+    </>
   );
 };
 
