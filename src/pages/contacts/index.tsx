535310814bf8efff8a5dccf88be24586c4b98706
gml9812 <hee.jun.yun@gmail.com>
2022-02-15 20:47:55 +0900

    refactor: useEffect를 getStaticProps로 교체
    - Q&A의 경우 내용이 바뀔 일이 거의 없기에 static한 fetch 방식을
      사용하도록 변경

--- src/pages/contacts/index.tsx
+++ src/pages/contacts/index.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { ContentsDivider, CustomerServiceInfo, Faq, MenuSelector } from 'components';
 import { API_END_POINT } from 'commons';
@@ -19,89 +20,29 @@ export const Container = styled.div`
   flex-direction: column;
 `;
 
-// 임시
-export const Navbar = styled.div`
-  line-height: 1.15;
-  -webkit-text-size-adjust: 100%;
-  font-size: 15px;
-  font-family: Apple SD Gothic Neo, sans-serif;
-  font-weight: 600;
-  color: #000000;
-  -webkit-box-direction: normal;
-  box-sizing: border-box;
-  position: fixed;
-  display: flex;
-  background-color: #fff;
-  height: 59px;
-  z-index: 50;
-  width: 373px;
-  max-width: 48rem;
-  border-bottom: 1px solid transparent;
-  padding: 0px;
-  margin: 0px;
-`;
-
-interface QaLists {
-  [key: number]: [
-    {
-      id: number;
-      question: string;
-      answer: string;
-    },
-  ];
+interface IContactsProps {
+  qaTypes: {
+    id: number;
+    name: string;
+  }[];
+  qaLists: {
+    [key: number]: [
+      {
+        id: number;
+        question: string;
+        answer: string;
+      },
+    ];
+  };
 }
 
-const Contacts = () => {
-  const [qaTypes, setQaTypes] = useState([
-    {
-      id: 1,
-      name: '',
-    },
-  ]);
-  const [qaLists, setQaLists] = useState<QaLists>({
-    1: [
-      {
-        id: 1,
-        question: '',
-        answer: '',
-      },
-    ],
-  });
-  const [chosenTypeId, setChosenTypeId] = useState<number>(0);
-
-  // custom hook으로 만들기?
-  // 에러처리
-  useEffect(() => {
-    const fetchData = async () => {
-      const qaTypesResult = await axios.get(API_END_POINT.QA_TYPES);
-      const qaTypesData = qaTypesResult.data;
-      setQaTypes(
-        qaTypesData.qaTypes.map((qaType: Qa) => ({
-          id: qaType.id,
-          name: qaType.name,
-        })),
-      );
-      setChosenTypeId(qaTypesData.qaTypes[0].id); // chosenTypeId 초기화
-
-      const temp = await Promise.all(
-        qaTypesData.qaTypes.map(async (qaType: Qa) => {
-          return axios.get(`${API_END_POINT.QA_LISTS}${qaType.id}`);
-        }),
-      );
-
-      const qaListsData = temp.reduce((acc, current, idx) => {
-        return { ...acc, [qaTypesData.qaTypes[idx].id]: current.data.qas };
-      }, {});
-
-      setQaLists(qaListsData);
-    };
-
-    fetchData();
-  }, []);
+const Contacts = ({ qaTypes, qaLists }: IContactsProps) => {
+  const [chosenTypeId, setChosenTypeId] = useState<number>(qaTypes[0].id);
+  const router = useRouter();
 
   return (
     <>
-      <Navbar />
+      {/* <NavBar /> */}
       <Container>
         <CustomerServiceInfo />
         <ContentsDivider />
@@ -119,3 +60,29 @@ const Contacts = () => {
 };
 
 export default Contacts;
+
+// 에러처리
+export const getStaticProps = async () => {
+  const qaTypesResult = await axios.get(API_END_POINT.QA_TYPES);
+  const qaTypesData = qaTypesResult.data.qaTypes.map((qaType: Qa) => ({
+    id: qaType.id,
+    name: qaType.name,
+  }));
+
+  const temp = await Promise.all(
+    qaTypesData.map(async (qaType: Qa) => {
+      return axios.get(`${API_END_POINT.QA_LISTS}${qaType.id}`);
+    }),
+  );
+
+  const qaListsData = temp.reduce((acc, current, idx) => {
+    return { ...acc, [qaTypesData[idx].id]: current.data.qas };
+  }, {});
+
+  return {
+    props: {
+      qaTypes: qaTypesData,
+      qaLists: qaListsData,
+    },
+  };
+};
