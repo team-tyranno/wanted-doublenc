050a68e22450726e40e7865a67dd66b283d4e1fe
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-02-15 19:29:44 +0900

    feat: 상품 라우팅 처리

--- src/components/atoms/BrandButton/index.tsx
+++ src/components/atoms/BrandButton/index.tsx
@@ -1,10 +1,20 @@
 import { IBrandProps } from 'types';
+import { useRouter } from 'next/router';
 import * as S from './style';
 
-export const BrandButton = ({ data }: { data: IBrandProps }) => {
-  const { name, imageUrl } = data;
+export const BrandButton = ({ query, data }: { query: string; data: IBrandProps }) => {
+  const router = useRouter();
+  const { id, name, imageUrl } = data;
+
+  const onClick = () => {
+    router.push({
+      pathname: `/brands/${id}`,
+      query: { id: query },
+    });
+  };
+
   return (
-    <S.Container>
+    <S.Container onClick={onClick}>
       <img src={imageUrl} alt={`${name} 로고`} />
       <div>{name}</div>
     </S.Container>
