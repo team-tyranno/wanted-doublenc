63981612e66dbbc492d0159cff47e9c043823c33
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-02-15 21:08:37 +0900

    feat: 아이템 리스트 컴포넌트 적용

---
+++ src/components/molecules/ItemLists/style.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  margin-top: 9px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  background: #fff;
+`;
