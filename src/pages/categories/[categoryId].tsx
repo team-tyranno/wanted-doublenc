c7b409db2448bae828b73fd08c61fd9147ea0309
Yeum Sang Kwon <kunshup2000@gmail.com>
2022-02-15 23:02:43 +0900

    feat: 데이터 패칭 Promise.all로  변경

--- src/pages/categories/[categoryId].tsx
+++ src/pages/categories/[categoryId].tsx
@@ -1,13 +1,26 @@
-import { getCategoryPath, getCategotyData } from 'utils';
-import { ICafeProps, IPathProps } from 'types';
-import { BrandHeader, BrandLists } from 'components';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import { getCategoryPath, getCategotyData, getMenuList } from 'utils';
+import { ICafeProps, IPathProps, IMenuProps, ICategoryDetailProps } from 'types';
+import { BrandHeader, BrandLists, MenuSelector } from 'components';
 
-const CategoryId = ({ datas }: { datas: ICafeProps }) => {
+const CategoryId = ({ datas, menus }: { datas: ICafeProps; menus: Array<IMenuProps> }) => {
+  const router = useRouter();
   const { id, name, conCategory2s } = datas;
 
+  const [selected, setIsSelected] = useState<number>(id);
+
+  const onClick = (key: number) => {
+    router.push(`/categories/${id}`);
+    setIsSelected(key);
+  };
+
+  const parseMenu = menus.flat() as unknown as Array<ICategoryDetailProps>;
   return (
     <>
-      <BrandHeader title={name} />
+      <BrandHeader title={name}>
+        <MenuSelector menuList={parseMenu} selected={selected} onClick={onClick} />
+      </BrandHeader>
       <BrandLists query={id.toString()} datas={conCategory2s} />
     </>
   );
@@ -15,11 +28,12 @@ const CategoryId = ({ datas }: { datas: ICafeProps }) => {
 
 export async function getStaticProps({ params }: { params: IPathProps }) {
   const { categoryId } = params;
-  const datas = await getCategotyData(categoryId);
 
+  const [datas, menus] = await Promise.all([getCategotyData(Number(categoryId)), getMenuList()]);
   return {
     props: {
       datas,
+      menus,
     },
   };
 }
